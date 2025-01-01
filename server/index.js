42b736be94ba4824dd15b1ede823ce3c0b108cb2
Kalp <kalp@Kalps-MacBook-Air.local>
2025-01-02 01:01:18 +0530

    ..

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import connectDB from "./db/db.js"
+import dotenv from 'dotenv'
+import bodyparser from "body-parser"
+import userRouter from "./routes/user.js"
+import todoRouter from "./routes/todo.js"
+import cookieParser from "cookie-parser"
+
+const app = express()
+dotenv.config()
+connectDB()
+const PORT = process.env.PORT || 3000
+
+app.use(cookieParser())
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use("/api/v1/user",userRouter)
+app.use("/api/v1/todo",todoRouter)
+
+app.get("/home",(req,res)=>{
+    res.send("Hi there")
+})
+app.listen(PORT,()=>{
+    console.log(`Server listen at port ${process.env.PORT}`)
+})
---
+++ server/routes/user.js
@@ -0,0 +1,11 @@
+import express from "express"
+import {logout, register} from "../controllers/user.js"
+import { login } from "../controllers/user.js"
+const router = express.Router()
+
+router.route("/register").post(register)
+router.route("/login").post(login)
+router.route("/logout").get(logout)
+
+
+export default router
