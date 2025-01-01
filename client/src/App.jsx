9a74d2cf560d44f10c9ab9547d135879d889af34
Kalp <kalp@Kalps-MacBook-Air.local>
2025-01-02 00:59:06 +0530

    creating init app with configuring shadcn with reactjs

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <Button>Lets build todo App</Button>
+    </div>
+  )
+}
+
+export default App
